0bc9127ec6f16ec9798604d13c2eb454554bc763
Shane Drabing <shane.drabing@gmail.com>
2022-03-29 15:30:00 -0600

    new private folder

--- push_app.R
+++
@@ -1,7 +0,0 @@
-rsconnect::setAccountInfo(
-    name = "shanedrabing",
-    token = "E146F010FB6CD871F4EBF2E3B115037B",
-    secret = readLines("data/secret.txt")[1]
-)
-
-rsconnect::deployApp("wikitree")
