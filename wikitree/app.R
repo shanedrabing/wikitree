dd887fcef0a2be69477e4b8b0286537a3f758255
Shane Drabing <shane.drabing@gmail.com>
2022-04-22 01:06:27 -0600

    danger zone features

--- wikitree/app.R
+++ wikitree/app.R
@@ -775,8 +775,7 @@ ui <- fluidPage(
                 textAreaInput("text_in", NULL, DEFAULT_INPUT, "100%", "60vh", resize = "vertical")
             ),
             tabPanel("Config",
-                 br(), p("General Options", style = "text-align:center;font-weight:bold;"),
-                checkboxInput("explore", "Explore?", FALSE),
+                br(), p("General Options", style = "text-align:center;font-weight:bold;"),
                 checkboxInput("simplify", "Simplify?", TRUE),
                 fixedRow(column(4, p("Recursive Search")), column(8,
                     sliderInput("crawl", NULL, 1, 5, 1, animate = FALSE)
@@ -794,7 +793,9 @@ ui <- fluidPage(
                 )),
                 fixedRow(column(4, p("Rankdir")), column(8,
                     selectInput("rankdir", NULL, OPTIONS_RANKDIR)
-                ))
+                )),
+                br(), p("Danger Zone", style = "text-align:center;font-weight:bold;"),
+                checkboxInput("explore", "Explore?", FALSE)
             )
         ), width = 2),
         column(tabsetPanel(type = "pills",
